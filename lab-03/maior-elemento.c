7deed344dd5455afca5a97b703bd85929814ef25
luana <luanaribeiropalhares@gmail.com>
2024-04-18 14:14:20 -0300

    update maior-elemento.c

--- lab-03/maior-elemento.c
+++ lab-03/maior-elemento.c
@@ -8,14 +8,11 @@ int main()
     for (int i = 0; i < lin; i++)
     {
         for (int j = 0; j < col; j++)
+        {
             scanf("%d", &matriz[i][j]);
-    }
-    maior = matriz[0][0];
-    for (int i = 0; i < lin; i++)
-    {
-        for (int j = 0; j < col; j++)
-            if (matriz[i][j] > maior)
+            if ((i == 0 && j == 0) || (matriz[i][j] > maior))
                 maior = matriz[i][j];
+        }
     }
     printf("%d", maior);
 }
